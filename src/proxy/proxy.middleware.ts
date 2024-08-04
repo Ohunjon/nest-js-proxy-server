7d04a628a742bc641d398bee618e434619ba18bd
Ohunjon <ohun_jon96@mail.ru>
2024-08-04 17:24:33 +0500

    test

--- src/proxy/proxy.middleware.ts
+++ src/proxy/proxy.middleware.ts
@@ -5,9 +5,12 @@ import {
 } from 'http-proxy-middleware';
 import { Response, Request } from 'express';
 import { JSDOM } from 'jsdom';
+import { json } from 'stream/consumers';
+import axios from 'axios';
 
 @Injectable()
 export class ProxyMiddleware implements NestMiddleware {
+
   processTextNodes(node: any) {
     const TEXT_NODE_TYPE = 3;
 
@@ -16,7 +19,7 @@ export class ProxyMiddleware implements NestMiddleware {
       const text = node.textContent.trim();
 
       if (parentNode.nodeName !== 'STYLE' && parentNode.nodeName !== 'SCRIPT') {
-        if (text && text.length === 6) {
+        if (text && text.length > 6) {
           node.textContent = text + '™';
         }
       }
@@ -29,7 +32,7 @@ export class ProxyMiddleware implements NestMiddleware {
   }
 
   use(req: Request, res: any, next: () => void) {
-    let targetLink = 'https://somon.tj';
+    let targetLink = 'https://docs.nestjs.com/';
     if (req.originalUrl) {
       targetLink = targetLink + req.originalUrl;
     }
@@ -37,37 +40,78 @@ export class ProxyMiddleware implements NestMiddleware {
     const proxy = createProxyMiddleware({
       target: `${targetLink}`,
       changeOrigin: true,
-      secure: false,
+      secure: true,
+      // timeout: 5000, // Timeout for the proxy request in milliseconds
+      // proxyTimeout: 5000,
       selfHandleResponse: true,
       on: {
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
         proxyReq: (proxyReq, _req: Request, _res: Response) => {
           /* handle proxyReq */
           console.log('Proxying request to:', proxyReq.path);
         },
         proxyRes: responseInterceptor(
           async (responseBuffer, proxyRes, _req, res: Response) => {
-            res.removeHeader('content-security-policy');
-            const contentType = proxyRes.headers['content-type'];
-            if (!contentType?.startsWith('image/')) {
-              const response = responseBuffer.toString('utf8');
-              const dom = new JSDOM(response);
+            console.log(`Received response with status: ${proxyRes.statusCode}`);
+            // res.removeHeader('content-security-policy');
+            // const contentType = proxyRes.headers['content-type'];
+            // if (!contentType?.startsWith('image/')) {
+            // const response = responseBuffer.toString('utf8');
+            // console.log(responseBuffer.toString('utf-8'));
+            // const dom = new JSDOM(response);
 
-              const document = dom.window.document;
-              const documentOrg = dom.window.document;
-              let bodyContent = document.querySelector('body');
+            // const document = dom.window.document;
+            // const documentOrg = dom.window.document;
+            // let bodyContent = document.querySelector('body');
 
-              if (bodyContent) {
-                bodyContent = this.processTextNodes(bodyContent);
-              }
+            // if (bodyContent) {
+            //   //  bodyContent = this.processTextNodes(bodyContent);
+            // }
 
-              const oldBody = documentOrg.querySelector('body');
-              oldBody.parentNode.replaceChild(bodyContent, oldBody);
+            // const oldBody = documentOrg.querySelector('body');
+            // oldBody.parentNode.replaceChild(bodyContent, oldBody);
 
-              return dom.serialize();
-            }
-            const statusCode = res.statusCode === 200 ? res.statusCode : 500;
-            res.status(statusCode);
+            //return dom.serialize();
+            //}
+            // const statusCode = res.statusCode === 200 ? res.statusCode : 500;
+            //res.status(statusCode);
+            // return JSON.stringify(responseBuffer);
+
+            // const responseStringg = responseBuffer.toString('utf8');
+            // const dom = new JSDOM(responseStringg);
+            // const document = dom.window.document;
+            // let responseString = document.body.innerHTML;
+            // const responseString = bodyContent.toString('utf8');
+
+            // res.setHeader('Content-Type', 'application/json');
+
+            // Return the modified response string
+            // return this.processTextNodes(document.querySelector('body')).innerHTML;
+            // Add a script to send a request after all resources are loaded
+
+            //return responseString;
+
+            let data = '';
+
+
+            proxyRes.on('data', (chunk: any) => {
+              console.log('on data')
+              data += chunk;
+            });
+
+            proxyRes.on('end', async () => {
+
+              //  const responseStringg = responseBuffer.toString('utf8');
+              // const dom = new JSDOM(data);
+              // const document = dom.window.document;
+              // Wait for all resources to be fetched
+
+              // let responseString = document.body.innerHTML;
+              //  await this.fetchAllResources(document);
+              // let responseString = document.body.innerHTML;
+              console.log('on data end');
+            });
+
             return responseBuffer;
           }
         ),
@@ -75,15 +119,37 @@ export class ProxyMiddleware implements NestMiddleware {
         error: (_err, _req, res: Response) => {
           console.log('error');
 
-          res.writeHead(500, {
-            'Content-Type': 'aplication/json'
-          });
-          res.end(
-            'Something went wrong. And we are reporting a custom error message.'
-          );
+          if (!res.headersSent) {
+            res.status(500).send('Proxy error');
+          }
         }
       }
     });
     proxy(req, res, next);
+  }
+
+  async fetchAllResources(document) {
+    const promises = [];
+    const resources = document.querySelectorAll('img[src], link[href], script[src]');
+
+    resources.forEach(resource => {
+      const url = resource.src || resource.href;
+
+      if (url) {
+        promises.push(
+          axios.get(url)
+            .then(response => {
+              console.log(`Fetched resource: ${url}`);
+              return response.data;
+            })
+            .catch(error => {
+              console.error(`Error fetching resource: ${url}`, error);
+            })
+        );
+      }
+    });
+
+    // Wait for all fetch requests to complete
+    await Promise.all(promises);
   }
 }
